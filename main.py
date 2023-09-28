0ec53b5938bfbbde4970ff38e577f21db8997ad4
Nate Rathbun <naterathbun@gmail.com>
2023-09-27 20:31:00 -0500

    Added input for zip code and formatted output

--- main.py
+++ main.py
@@ -1,22 +1,27 @@
 import http.client
+import json
 
 apiKey = 'x'
 urlBase = 'https://weatherapi-com.p.rapidapi.com/current.json?q='
 apiHost = 'weatherapi-com.p.rapidapi.com'
 
 connection = http.client.HTTPSConnection(apiHost)
+headers = {'X-RapidAPI-Key': apiKey, 'X-RapidAPI-Host': apiHost}
 
-headers = {
-    'X-RapidAPI-Key': apiKey,
-    'X-RapidAPI-Host': apiHost
-}
-
-zipCode = '60565' # for testing
+zipCode = input('Enter a 5 digit US zip code:')
 
 fullUrl = urlBase + zipCode
+
 connection.request("GET", fullUrl, headers=headers)
+response = connection.getresponse()
+data = response.read()
 
-res = connection.getresponse()
-data = res.read()
+weatherReport = json.loads(data.decode("utf-8"))
 
-print(data.decode("utf-8"))
+print('Weather Report for ' + weatherReport["location"]["name"] + ', ' + weatherReport["location"]["region"] + ':')
+print('=========================================')
+print('Conditions: ' + weatherReport['current']['condition']['text'])
+print('Temperature: ' + str(weatherReport['current']['temp_f']) + ' Degrees F, but feels like ' + str(weatherReport['current']['feelslike_f']))
+print('Humidity: ' + str(weatherReport['current']['humidity']) + '%')
+print('Winds: ' + str(weatherReport['current']['wind_mph']) + ' mph out of the ' + weatherReport['current']['wind_dir'] + ' with gusts up to ' + str(weatherReport['current']['gust_mph']) + ' mph')
+print('Barometric Pressure: ' + str(weatherReport['current']['pressure_in']) + ' in.')
